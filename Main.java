548a3bd25961e78ef23b5d36ed3011ca6756fe74
sylram <sylram7@gmail.com>
2019-02-05 17:31:53 -0500

    test Bob and Alice

--- Main.java
+++ Main.java
@@ -1,9 +1,21 @@
 /**
  * Created by iyasuwatts on 10/17/17.
  */
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args ){
-
-    }
+        String name;
+        System.out.println("Enter your name: ");
+        Scanner input = new Scanner(System.in);
+        name = input.nextLine();
+        if(name.equals("Bob")||name.equals("Alice")){
+        System.out.println("Hello "+name+" how are you today?");
+        
+    } else {System.out.println("You're not Bob or Alice");
+};
 }
+}
+        
+    
+
